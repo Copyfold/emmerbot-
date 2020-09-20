fd254a85441b20624fbf47ffe9bd73e423b16878
Emi <copyfold@gmail.com>
2020-09-19 20:34:56 -0600

    update

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ def generate() -> str:
 @bot.event
 async def on_ready() -> None:
     await bot.change_presence(
-        status=discord.Status.online, activity=discord.Game("Eating Bacon")
+        status=discord.Status.online, activity=discord.Game("with humans!")
     )
 
 
